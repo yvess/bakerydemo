f0089801081103af8b1a5483f5ed4a427548ac14
Karl Hobley <karl@torchbox.com>
2020-07-02 21:32:31 +0100

    Ok, now it should all work

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 # Copy SSH private key to file, if set
 # This is used for talking to GitHub over an SSH connection
 echo $SSH_PRIVATE_KEY | base64 --decode > $HOME/.ssh/id_rsa
-# ssh -oStrictHostKeyChecking=no -T github.com
+ssh -oStrictHostKeyChecking=no -T github.com
 
 exec "$@"
