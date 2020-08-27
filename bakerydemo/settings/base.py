0c7c080d6d908492e3630be80996fc086ce583a3
Karl Hobley <karl@torchbox.com>
2020-08-27 12:40:19 +0100

    Add styleguide

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'wagtail.api.v2',
     'wagtail.contrib.modeladmin',
     'wagtail.contrib.routable_page',
+    'wagtail.contrib.styleguide',
     'wagtail.core',
 
     'rest_framework',
